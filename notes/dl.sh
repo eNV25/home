26e1b494ccac7d65652c060bd16582312c5642af
eNV25 <env252525@gmail.com>
2022-05-01 10:07:01 +0530

    Sun, 01 May 2022 10:07:01 +0530

---
+++ notes/dl.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+tput civis
+trap '
+	tput cvvis
+	tput cr el
+' 0
+/usr/bin/aria2c \
+	--split=5 \
+	--max-connection-per-server=5 \
+	--max-concurrent-downloads=5 \
+	--file-allocation=falloc \
+	--remote-time=true \
+	--conditional-get=true \
+	--no-netrc=true \
+	\
+	--continue=true \
+	--allow-overwrite=true \
+	--always-resume=false \
+	\
+	--download-result=hide \
+	--summary-interval=0 \
+	--console-log-level=warn \
+	--log-level=warn \
+	-UWget \
+	"$1" -o "$2"

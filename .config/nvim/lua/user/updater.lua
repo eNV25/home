08c402f571103255cc64948436300abf65931c7d
eNV25 <env252525@gmail.com>
2023-04-18 10:12:45 +0200

    Tue, 18 Apr 2023 10:12:45 +0200

--- .config/nvim/lua/user/updater.lua
+++ .config/nvim/lua/user/updater.lua
@@ -1,13 +1,18 @@
 require("astronvim.utils.updater").update = function()
   local repo = "https://github.com/AstroNvim/AstroNvim.git"
   local tag = string.match(
-    vim.fn.system({ "git", "-c", "versionsort.suffix=-", "ls-remote", "--tags", "--sort=v:refname", repo }),
+    vim.fn.system({ "git", "-c", "versionsort.suffix=-", "ls-remote", "--tags", "--sort=version:refname", repo }),
     "refs/tags/(v%d+.%d+.%d+)\r?\n$"
   )
-  local HOME = vim.env.HOME
   print(vim.fn.system({
-    "git", "--git-dir", HOME .. "/.dotgit/", "--work-tree", HOME,
-    "subtree", "--prefix", ".config/nvim",
-    "pull", "--squash", repo, tag,
+    "flock", "--", vim.env.XDG_RUNTIME_DIR .. "/astronvim-subtree-update.lock", "sh", "-xc",
+    table.concat({
+      [[export GIT_DIR="$HOME/.dotgit"]],
+      [[export GIT_WORK_TREE="$HOME"]],
+      [[cd "$GIT_WORK_TREE"]],
+      [[git stash push]],
+      [[git subtree --prefix .config/nvim pull --squash ]] .. repo .. " " .. tag,
+      [[git stash pop]]
+    }, "\n")
   }))
 end

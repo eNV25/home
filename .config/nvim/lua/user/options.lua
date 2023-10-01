98eb645de828b9ae1611aca4b913d7117771003b
eNV25 <env252525@gmail.com>
2023-10-01 23:54:21 +0200

    Sun, 01 Oct 2023 23:54:21 +0200

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -8,5 +8,20 @@ return {
 	g = {
 		inlay_hints_enabled = true,
 		ui_notifications_enabled = false,
+		clipboard = (function()
+			if vim.env.WAYLAND_DISPLAY and vim.fn.executable("wl-copy") and vim.fn.executable("wl-paste") then
+				return {
+					name = "wl-copy",
+					copy = {
+						["+"] = { "wl-copy", "--type", "text/plain" },
+						["*"] = { "wl-copy", "--primary", "--type", "text/plain" },
+					},
+					paste = {
+						["+"] = { "wl-paste", "--no-newline" },
+						["*"] = { "wl-paste", "--no-newline", "--primary" },
+					},
+				}
+			end
+		end)(),
 	},
 }

a4f1b0a7c9af14e52f9c326b0f55d321398d1ae8
eNV25 <env252525@gmail.com>
2023-11-17 15:15:42 +0100

    Fri, 17 Nov 2023 15:15:42 +0100

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,31 +4,37 @@ local function config_home()
   return os.getenv("XDG_CONFIG_HOME") or (os.getenv("HOME") .. "/.config")
 end
 
+local font = wezterm.font_with_fallback({
+  {
+    family = "Monaspace Krypton",
+    harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "dlig" },
+  },
+  "Symbols Nerd Font",
+  "Noto Color Emoji",
+})
+local color_scheme = "github_dark_dimmed"
+local color_schemes = {}
+setmetatable(color_schemes, {
+  __index = function(_, key)
+    local scheme = wezterm.color.load_scheme(config_home() .. "/wezterm/colors/" .. key .. ".toml")
+    scheme.tab_bar = {
+      background = scheme.background,
+      active_tab = { bg_color = scheme.selection_bg, fg_color = scheme.selection_fg },
+      inactive_tab = { bg_color = scheme.background, fg_color = scheme.foreground },
+      inactive_tab_hover = { bg_color = scheme.foreground, fg_color = scheme.background },
+      new_tab = { bg_color = scheme.background, fg_color = scheme.foreground },
+      new_tab_hover = { bg_color = scheme.foreground, fg_color = scheme.background },
+    }
+    return scheme
+  end,
+})
+color_schemes[color_scheme] = color_schemes[color_scheme]
+
 return {
   default_prog = { os.getenv("SHELL") },
-  font = wezterm.font_with_fallback({
-    {
-      family = "Monaspace Krypton",
-      harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "dlig" },
-    },
-    "Symbols Nerd Font",
-    "Noto Color Emoji",
-  }),
-  color_scheme = "github_dark_dimmed",
-  color_schemes = {
-    ["github_dark_dimmed"] = (function()
-      local scheme = wezterm.color.load_scheme(config_home() .. "/wezterm/colors/github_dark_dimmed.toml")
-      scheme.tab_bar = {
-        background = scheme.background,
-        active_tab = { bg_color = scheme.selection_bg, fg_color = scheme.selection_fg },
-        inactive_tab = { bg_color = scheme.background, fg_color = scheme.foreground },
-        inactive_tab_hover = { bg_color = scheme.foreground, fg_color = scheme.background },
-        new_tab = { bg_color = scheme.background, fg_color = scheme.foreground },
-        new_tab_hover = { bg_color = scheme.foreground, fg_color = scheme.background },
-      }
-      return scheme
-    end)(),
-  },
+  font = font,
+  color_scheme = color_scheme,
+  color_schemes = color_schemes,
   use_fancy_tab_bar = false,
   alternate_buffer_wheel_scroll_speed = 1,
 }
